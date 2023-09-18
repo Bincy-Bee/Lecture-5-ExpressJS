47da777412f414dac571fe12a40187636a1637ac
Bhavin Patel <patelbhavin7784@gmail.com>
2023-09-18 13:59:42 +0530

    Expres JS server and Method GET & POST

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    console.log(req.body);
+    res.status(200).send("Welcome to express Server")
+})
+
+app.get("/login", (req, res)=>{
+    console.log(req.body)
+    res.status(200).send("Welcome to Login page via express server")
+})
+
+app.post("/signup", (req, res)=>{
+    console.log(req.body);
+    res.status(200).send(req.body)
+})
+
+app.post("/cart",(req, res)=>{
+    console.log(req.body);
+    res.status(200).send(req.body)
+})
+app.post("/login", (req, res)=>{
+    console.log(req.body);
+    res.status(200).send(req.body)
+})
+
+app.listen(8090,()=>{
+    console.log("Express Server is listing on port http://localhost:8090");
+})
